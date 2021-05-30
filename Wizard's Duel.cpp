22ebf7e7a2aded5c5861ca4258075e72ad5e47f9
Manav Verma <manavverma51@gmail.com>
2021-05-30 20:15:29 +0530

    Create Wizard's Duel.cpp

---
+++ Wizard's Duel.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    double x,ans,a,b,speed;
+  
+    while(cin>>x>>a>>b){
+        speed=x/(a+b);
+        ans=spd*a;
+        cout<<ans<<endl;
+    }
+    return 0;
+}
