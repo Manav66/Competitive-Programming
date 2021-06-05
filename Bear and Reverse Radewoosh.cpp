d73f9eca78158adfbe201c400c58d24026bf15c5
Manav Verma <manavverma51@gmail.com>
2021-06-05 19:12:47 +0530

    Create Bear and Reverse Radewoosh.cpp

---
+++ Bear and Reverse Radewoosh.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,c,times1=0,times2=0;
+    int score[1010],time[1010];
+    cin>>n>>c;
+ 
+    for(int i=0; i<n; i++)
+        cin>>score[i];
+    for(int i=0; i<n; i++)
+        cin>>time[i];
+ 
+    int limak=0,radoewoosh=0;
+ 
+    for(int i=0; i<n; i++){
+        times1+=time[i];
+        limak+=max(0,score[i]-c*times1);
+    }
+ 
+    sort(score,score+n, greater<int>());
+    sort(time,time+n, greater<int>());
+    for(int i=0; i<n; i++){
+        times2+=time[i];
+        radoewoosh+=max(0,score[i]-c*times2);
+    }
+
+    if(limak>radoewoosh){
+        cout<<"Limak\n";
+    }
+    else if(limak<radoewoosh){
+        cout<<"Radewoosh\n";
+    }
+    else if(limak==radoewoosh){
+        cout<<"Tie\n";
+    }
+ 
+    return 0;
+}
