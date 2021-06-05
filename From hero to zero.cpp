7e59a03ba4f0502b9ce0409f9acb0ede2bee9170
Manav Verma <manavverma51@gmail.com>
2021-06-05 19:23:15 +0530

    Create From hero to zero.cpp

---
+++ From hero to zero.cpp
@@ -0,0 +1,30 @@
+#include<cstdio>
+#include<iostream>
+#include<set>
+using namespace std;
+ int main()
+ {
+     int t;
+     scanf("%d",&t);
+     while(t--)
+     {
+         long long n;
+         long long k;
+         long long ans = 0;
+         scanf("%lld %lld",&n,&k);
+         while(n)
+         {
+             if(n % k)
+             ans += n%k, n = n/k*k;
+             else
+             {
+                 ans++;
+                 n /= k;
+             }
+ 
+         }
+         cout << ans << endl;
+     }
+ 
+     return 0;
+ }
